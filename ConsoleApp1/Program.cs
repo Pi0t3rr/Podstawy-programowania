cd81a34cbcd3b55a4b7fbf0a89b73a1160690733
KIE_Admin <KIE_Admin@WZR-C24-18>
2025-03-03 15:22:25 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{ /// <summary>
+/// Metoda startowa
+/// </summary>
+/// <param name="args">argumenty metody </param>
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("witaj świecie"); //wypisuje na ekranie tekst
+            Console.ReadKey(true);
+            /*         
+            Console.ReadKey(true);
+            Console.ReadKey(true);
+            Console.ReadKey(true);
+            */
+        }
+    }
+}
