7172e7fde4c707e0447b53a6e86488053117475f
Pi0t3rr <piotrpusz2006@gmail.com>
2025-03-03 15:33:45 +0100

    koniec zadania 1

---
+++ Zadanie 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisac program wypisujacy na ekranie tekst "witaj swiecie"
+            Console.WriteLine("witaj świecie");
+            Console.ReadKey(true);
+               }
+    }
+}
