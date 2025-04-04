b190143ae53cf7140b830a97d0c96e8f6e1091f1
Piotr Puszczewicz <p.puszczewicz.047@studms.ug.edu.pl>
2025-04-04 14:31:16 +0200

    spojrzec na konstrukcje do while

---
+++ Zadanie 3 while/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Napisz program wczytujący dwie liczby całkowite a oraz b(a < b) i obliczający sumę kolejnych
+            //liczb całkowitych począwszy od liczby a, a skończywszy na liczbie b. Obliczoną sumę wypisz
+            //na ekranie.Użyj pętli while.Np.dla a = -2 i b = 5 obliczamy sumę(-2)+(-1) + 0 + 1 + 2 + 3 + 4 + 5.
+
+            int a, b, suma, i;
+            Console.WriteLine("podaj a (muis być większe od b): ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            suma = 0;
+            i = a;
+            while (i <= b)
+            {
+                suma += i;
+                i++;
+            }
+            Console.WriteLine("Suma kolejnych liczb całkowitych zaczynając od {0} i kończąc na {1} to: {2}", a, b, suma);
+            Console.ReadKey(true);
+
+
+
+
+            }
+
+        }
+    }
+
---
+++ Zadanie 5 while/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_5_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Napisać program wczytujący od użytkownika liczbę rzeczywistą x. Wczytuj tak długo wartość
+            //zmiennej x, aż przyjmie ona wartość dodatnią. Wczytaną dodatnią wartość liczby x wypisz na
+            //ekranie.Użyj pętli do -while.
+
+
+            double x;
+
+            do
+            {
+                Console.WriteLine("Podaj x: ");
+                x = Convert.ToDouble(Console.ReadLine());
+            }
+            while (x <= 0);
+            Console.WriteLine("Twoja liczba więszka od 0 to {0}", x);
+            
+
+
+                Console.ReadKey(true);
+        }
+
+
+        
+
+        }
+    }
+
