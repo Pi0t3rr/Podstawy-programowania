53f7f2eece67c91deec22e38ec47f3edcf620992
Kubson147 <k.dolo123@gmail.com>
2025-04-14 14:24:06 +0200

    skonczylem 3 zadania

---
+++ Zadanie 1 metody funkcje/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1_metody_funkcje
+{
+    internal class Program
+    {
+        
+        
+            //            Napisz funkcję o nazwie suma wyznaczającą sumę jej dwóch argumentów będących liczbami
+            //całkowitymi.Napisz program wykorzystujący funkcję suma.
+
+            static int suma(int a, int b)
+            {
+                return a + b;
+            }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3+8={0} ", suma(3, 8));
+            Console.ReadKey(true);
+
+
+
+            }
+        
+    }
+}
---
+++ Zadanie 2 metody funkcje/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2_metody_funkcje
+{
+    internal class Program
+    {
+            //Napisz funkcję o nazwie max, która wyznacza maksimum z dwóch liczb rzeczywistych
+            //zadanych jako argumenty tej funkcji.Napisz program wykorzystujący funkcję max
+
+        static double max(double a, double b)
+        {
+            if (a > b) return a;
+            else return b;
+
+
+        }
+        static void Main(string[] args) { 
+        Console.WriteLine("Max z 5 i 10 to: {0}", max (5, 10));
+        Console.WriteLine("Max z 10 i 5 to: {0}", max(10, 5));
+        Console.WriteLine("Max z 5 i 5 to: {0}", max(5, 5));
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
