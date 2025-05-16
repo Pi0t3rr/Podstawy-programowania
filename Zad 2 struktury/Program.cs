add1700769b63345740a581bb741d2b4b08aa347
zofiaszulakowska <KIE_Admin@WZR-C25-05>
2025-05-16 14:23:33 +0200

    koniec

---
+++ Zad 2 struktury/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_2_struktury
+{
+    struct Uczen
+    {
+        public string imie, nazwisko;
+        public byte[] oceny;
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Zdefiniuj strukturę o nazwie Uczen zawierającą pola: imie, nazwisko i tablice ocen.
+            //            Pobierz od użytkownika liczbę uczniów i zadeklaruj odpowiednią tablicę struktur
+            //umożliwiającą przechowanie informacji o imionach, nazwiskach i ocenach uczniów.
+            //Następnie pobierz dane od użytkownika i w sposób czytelny wypisz wszystko na ekranie.
+
+            Uczen[] uczniowie;
+            int ile_osob, ile_ocen, i, j;
+            Console.Write("Podaj ilość uczniów w klasie: ");
+            ile_osob = Convert.ToInt32(Console.ReadLine());
+            uczniowie = new Uczen[ile_osob];
+            for (i = 0; i < ile_osob; i++)
+            {
+                Console.Write("Podaj imię ucznia {0} ", i + 1);
+                uczniowie[i].imie = Console.ReadLine();
+                Console.Write("Podaj nazwisko ucznia {0} ", i + 1);
+                uczniowie[i].nazwisko = Console.ReadLine();
+                Console.Write("Ile ocen ma {0} {1}: ", uczniowie[i].imie, uczniowie[i].nazwisko);
+                ile_ocen = Convert.ToInt32(Console.ReadLine());
+                uczniowie[i].oceny = new byte[ile_ocen];
+
+                for (j = 0; j < ile_ocen; j++)
+                {
+                    Console.Write("Podaj ocene numer {0} ", j + 1);
+                    uczniowie[i].oceny[j] = Convert.ToByte(Console.ReadLine());
+                }
+
+                Console.WriteLine();
+
+            }
+            for (i = 0; i < ile_osob; i++)
+            {
+
+                Console.Write("{0}. {1} {2}: ", i + 1, uczniowie[i].imie, uczniowie[i].nazwisko);
+                foreach (byte o in uczniowie[i].oceny)
+                    Console.Write(" {0}, ", o);
+                Console.WriteLine("");
+
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
+
+   
+
+
+
+
+
+
+
+        
+    
+
---
+++ zad 1 struktury/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_1_struktury
+{
+
+    struct Osoba
+    {
+        public string imie, nazwisko;
+        public int rok_urodzenia;
+
+    }
+        
+
+
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            //            Zdefiniuj strukturę o nazwie Osoba posiadającą trzy pola: imie, nazwisko i rok_urodzenia.
+            //Zadeklaruj zmienną typu strukturalnego o nazwie o, wczytaj do niej wszystkie wartości pól od
+            //użytkownika i wypisz wszystkie pobrane wartości pól na ekranie.
+
+            Osoba x;
+            Console.WriteLine("Podaj imię: ");
+            x.imie = Console.ReadLine();
+            Console.WriteLine("Podaj nazwisko: ");
+            x.nazwisko = Console.ReadLine();
+            Console.WriteLine("Podaj rok urodzenia: ");
+            x.rok_urodzenia = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("osobnik {0} {1} urodził się w {2} roku.", x.imie, x.nazwisko, x.rok_urodzenia);
+            Console.ReadKey(true);
+
+
+            
+                
+
+
+        }   
+    }
+}
